af9ae01b1ce213606abecfc6ece14eeb3c564517
vagusX <vagusxl@gmail.com>
2020-02-20 17:50:11 +0800

    feat: copy code from compatible pack to avoid load front end code in nodejs

---
+++ transforms/utils/compatible-icon-utils.js
@@ -0,0 +1,73 @@
+const camelCase = require('lodash.camelcase');
+const upperFirst = require('lodash.upperfirst');
+
+// copied from https://github.com/ant-design/compatible/blob/master/src/icon/utils.ts
+// "translate" these code to commonjs to avoid `babel` and `regenerator-runtime`
+
+const themeMap = {
+  filled: 'filled',
+  outlined: 'outlined', // default theme
+  twoTone: 'twoTone',
+};
+
+// moved from https://github.com/ant-design/ant-design/blob/master/components/icon/utils.ts
+const fillTester = /-fill$/;
+const outlineTester = /-o$/;
+const twoToneTester = /-twotone$/;
+
+function withThemeSuffix(type, theme) {
+  const result = upperFirst(camelCase(type));
+  const realTheme = upperFirst(themeMap[theme]);
+
+  if (theme !== 'outlined' && !realTheme) {
+    console.warn(
+      false,
+      'Icon',
+      `This icon '${type}' has unknown theme '${theme}'`,
+    );
+  }
+
+  return result + realTheme;
+}
+
+function removeTypeTheme(type) {
+  return type
+    .replace(fillTester, '')
+    .replace(outlineTester, '')
+    .replace(twoToneTester, '');
+}
+
+// For alias or compatibility
+function alias(type) {
+  let newType = type;
+  switch (type) {
+    case 'cross':
+      newType = 'close';
+      break;
+    // https://github.com/ant-design/ant-design/issues/13007
+    case 'interation':
+      newType = 'interaction';
+      break;
+    // https://github.com/ant-design/ant-design/issues/16810
+    case 'canlendar':
+      newType = 'calendar';
+      break;
+    // https://github.com/ant-design/ant-design/issues/17448
+    case 'colum-height':
+      newType = 'column-height';
+      break;
+    default:
+  }
+  console.warn(
+    newType === type,
+    'Icon',
+    `Icon '${type}' was a typo and is now deprecated, please use '${newType}' instead.`,
+  );
+  return newType;
+}
+
+module.exports = {
+  withThemeSuffix,
+  removeTypeTheme,
+  alias,
+};

45d5807a66cdff9dda16c47483d765d65c3a2df1
vagusX <vagusX@users.noreply.github.com>
2020-02-28 00:15:53 +0800

    feat: v3-typings-to-compatible (#65)
    * feat: v3-typings-to-compatible
    
    * test: improve test cases

---
+++ transforms/__tests__/v3-typings-to-compatible.test.js
@@ -0,0 +1,56 @@
+jest.mock('../v3-typings-to-compatible', () => {
+  return Object.assign(require.requireActual('../v3-typings-to-compatible'), {
+    parser: 'babylon',
+  });
+});
+
+const defineSnapshotTest = require('jscodeshift/dist/testUtils')
+  .defineSnapshotTest;
+
+const testUnit = 'v3-typings-to-compatible';
+
+const transform = require(`../${testUnit}`);
+
+describe(testUnit, () => {
+  defineSnapshotTest(
+    transform,
+    {},
+    `import { FormComponentProps } from 'antd/es/form';`,
+    'basic es import',
+  );
+
+  defineSnapshotTest(
+    transform,
+    {},
+    `import { FormComponentProps } from 'antd/lib/form';`,
+    'basic lib import',
+  );
+
+  defineSnapshotTest(
+    transform,
+    {},
+    `import { FormComponentProps as AAFormProps } from 'antd/es/form';`,
+    'alias import from es',
+  );
+
+  defineSnapshotTest(
+    transform,
+    {},
+    `import { FormComponentProps as AAFormProps } from 'antd/lib/form';`,
+    'alias import from lib',
+  );
+
+  defineSnapshotTest(
+    transform,
+    {},
+    `import { FormComponentProps, FormItemProps } from 'antd/lib/form';`,
+    'basic import: multi',
+  );
+
+  defineSnapshotTest(
+    transform,
+    {},
+    `import { FormComponentProps as AAFormProps, FormItemProps as AAFormItemProps } from 'antd/lib/form';`,
+    'alias import: multi',
+  );
+});

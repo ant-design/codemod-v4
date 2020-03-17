551e3493377d05de2c7649a424bdae59ffd1c376
vagusX <vagusxl@gmail.com>
2020-03-17 12:42:59 +0800

    feat: add react version warning

--- bin/cli.js
+++ bin/cli.js
@@ -159,7 +159,7 @@ async function upgradeDetect(targetDir, needIcon, needCompatible) {
       result.push([
         'install',
         '@ant-design/icons',
-        pkgUpgradeList['@ant-design/icons'],
+        pkgUpgradeList['@ant-design/icons'].version,
       ]);
     }
 
@@ -167,7 +167,7 @@ async function upgradeDetect(targetDir, needIcon, needCompatible) {
       result.push([
         'install',
         '@ant-design/compatible',
-        pkgUpgradeList['@ant-design/compatible'],
+        pkgUpgradeList['@ant-design/compatible'].version,
       ]);
     }
   } else {
@@ -186,8 +186,9 @@ async function upgradeDetect(targetDir, needIcon, needCompatible) {
     // handle mustInstallOrUpgradeDeps
     mustInstallOrUpgradeDeps.forEach(depName => {
       let hasDependency = false;
+      const expectVersion = pkgUpgradeList[depName].version;
+      // const upgradePkgDescription = pkgUpgradeList[depName].description;
       dependencyProperties.forEach(property => {
-        const expectVersion = pkgUpgradeList[depName].version;
         const versionRange = _.get(packageJson, `${property}.${depName}`);
         // mark dependency installment state
         hasDependency = hasDependency || !!versionRange;
